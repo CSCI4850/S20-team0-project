bebfdd3b2acc8e99ed5107cd62b11a5d6593c441
Lucas Remedios PC <lucas.w.remedios@gmail.com>
2020-04-07 16:23:44 -0500

    funcs for paths to normalized data

--- utils/hgg_utils.py
+++ utils/hgg_utils.py
@@ -50,6 +50,50 @@ def get_each_hgg_folder( ):
     
     return sorted( [folder for folder in get_hgg_paths().iterdir() ] )
     
+def get_normalized_hgg_paths():
+    
+    """
+    This will only work if imported in a file that lives inside the S20-team0-project directory
+    Abbreviated expected directory setup for path to work:
+
+    ./some_directory_to_hold_everything/
+        +-- MICCAI_BraTS_2019_Data_Training
+        |
+        |   +-- MICCAI_BraTS_2019_Data_Training
+        |
+        |   |   + -- HGG
+        |
+        +-- S20-team0-project  */ Cloned github repo */
+        
+    Return path to HGG directory
+    """
+    
+    for path in sorted ( list( pathlib.Path.cwd().parent.iterdir() ) ):
+        if path.name == "MICCAI_BraTS_2019_Data_Training":
+            hgg_folders_path =  sorted(
+                                        list(
+                                            sorted(
+                                                    list(
+                                                        path.iterdir()
+                                                    )
+                                            )[0].iterdir()
+                                        )
+                                )[3]
+
+    return hgg_folders_path
+
+
+def get_each_normalized_hgg_folder( ):
+    
+    """
+    This will only work if imported in a file that lives inside the S20-team0-project directory
+    Abbreviated expected directory setup for path to work:
+
+    Returns a sorted list of paths to each HGG folder
+    """
+    
+    return sorted( [folder for folder in get_normalized_hgg_paths().iterdir() ] )
+    
 def get_scans_at_index( i ):
     
     """
